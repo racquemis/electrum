863762e8b292bd65a2f7731d60d52c6a74b0f56a
racquemis <racquemis@users.noreply.github.com>
2018-01-03 15:42:39 +0100

    Disabled multisig wallet selection

--- lib/base_wizard.py
+++ lib/base_wizard.py
@@ -79,9 +79,9 @@ class BaseWizard(object):
         ])
         wallet_kinds = [
             ('standard',  _("Standard wallet")),
-            ('2fa', _("Wallet with two-factor authentication")),
-            ('multisig',  _("Multi-signature wallet")),
-            ('imported',  _("Import Bitcoin addresses or private keys")),
+            #('2fa', _("Wallet with two-factor authentication")),
+            #('multisig',  _("Multi-signature wallet")),
+            ('imported',  _("Import Minexcoin addresses or private keys")),
         ]
         choices = [pair for pair in wallet_kinds if pair[0] in wallet_types]
         self.choice_dialog(title=title, message=message, choices=choices, run_next=self.on_wallet_type)
